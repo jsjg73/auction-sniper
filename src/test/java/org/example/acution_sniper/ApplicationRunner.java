ba6744d7c4ca473f1019f59a88aff185e61ab427
jsjg73 <kjs930703@gmail.com>
2025-02-20 22:50:57 +0900

    AuctionSniperEndToEndTest 스켈레톤

---
+++ src/test/java/org/example/acution_sniper/ApplicationRunner.java
@@ -0,0 +1,14 @@
+package org.example.acution_sniper;
+
+public class ApplicationRunner {
+    public void startBiddingIn(FakeAuctionServer auction) {
+        
+    }
+
+    public void showSniperHasLostAuction() {
+
+    }
+
+    public void stop() {
+    }
+}
---
+++ src/test/java/org/example/acution_sniper/FakeAuctionServer.java
@@ -0,0 +1,22 @@
+package org.example.acution_sniper;
+
+public class FakeAuctionServer {
+    public FakeAuctionServer(String itemId) {
+        
+    }
+
+    public void startSellingItem() {
+        
+    }
+
+    public void hasReceivedJoinRequestFromSniper() {
+        
+    }
+
+    public void announceClosed() {
+        
+    }
+
+    public void stop() {
+    }
+}
