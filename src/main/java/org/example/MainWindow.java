51b499a59d59b7df3adb60646acad44930570953
jsjg73 <kjs930703@gmail.com>
2025-02-21 23:06:42 +0900

    첫 테스트: 단일 품목: 참여,입찰하지 않은 상태로 낙찰 실패

--- src/main/java/org/example/MainWindow.java
+++ src/main/java/org/example/MainWindow.java
@@ -10,6 +10,7 @@ import static org.example.Main.MAIN_WINDOW_NAME;
 public class MainWindow extends JFrame {
     public static final String SNIPER_STATUS_NAME = "sniper status";
     public static final String STATUS_JOINING = "Joining";
+    public static final String STATUS_LOST = "Lost";
     private final JLabel sniperStatus = createLabel(STATUS_JOINING);
     public MainWindow() {
         super("Auction Sniper");
@@ -25,5 +26,9 @@ public class MainWindow extends JFrame {
         result.setName(SNIPER_STATUS_NAME);
         result.setBorder(new LineBorder(Color.BLACK));
         return result;
+    }
+
+    public void showStatus(String status) {
+        sniperStatus.setText(status);
     }
 }
