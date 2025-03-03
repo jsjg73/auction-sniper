3d1853225d928c9c4ce5dd2f1b27731ca96780db
jsjg73 <kjs930703@gmail.com>
2025-03-03 18:05:50 +0900

    15장 JLabel 대체 (2) page 175

--- src/main/java/org/example/MainWindow.java
+++ src/main/java/org/example/MainWindow.java
@@ -2,6 +2,7 @@ package org.example;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
+import javax.swing.table.AbstractTableModel;
 
 import java.awt.*;
 
@@ -14,14 +15,29 @@ public class MainWindow extends JFrame {
     public static final String STATUS_BIDDING = "Bidding";
     public static final String STATUS_WINNING = "Winning";
     public static final String STATUS_WON = "Won";
-    private final JLabel sniperStatus = createLabel(STATUS_JOINING);
+    private static final String SNIPERS_TABLE_NAME = "sniper table";
+    private final SnipersTableModel snipers = new SnipersTableModel();
+
     public MainWindow() {
         super("Auction Sniper");
         setName(MAIN_WINDOW_NAME);
-        add(sniperStatus);
+        fillContentPane(makeSnipersTable());
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    private void fillContentPane(JTable snipersTable) {
+        final Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        contentPane.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+    }
+
+    private JTable makeSnipersTable() {
+        final JTable snipersTable = new JTable(snipers);
+        snipersTable.setName(SNIPERS_TABLE_NAME);
+        return snipersTable;
     }
 
     private static JLabel createLabel(String initialText) {
@@ -31,7 +47,31 @@ public class MainWindow extends JFrame {
         return result;
     }
 
-    public void showStatus(String status) {
-        sniperStatus.setText(status);
+    public void showStatus(String statusText) {
+        snipers.setStatusText(statusText);
+    }
+
+    public class SnipersTableModel extends AbstractTableModel {
+        private String statusText = STATUS_JOINING;
+
+        @Override
+        public int getRowCount() {
+            return 1;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 1;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            return statusText;
+        }
+
+        public void setStatusText(String newStatusText) {
+            statusText = newStatusText;
+            fireTableRowsUpdated(0, 0);
+        }
     }
 }
