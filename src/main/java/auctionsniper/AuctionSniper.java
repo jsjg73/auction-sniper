c271860f1b93d9933f9e56a583ac3ade456396a8
jsjg73 <kjs930703@gmail.com>
2025-03-07 19:33:07 +0900

    AuctionSniperTest 정상화.

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -3,13 +3,13 @@ package auctionsniper;
 public class AuctionSniper implements AuctionEventListener {
     private final SniperListener sniperListener;
     private final Auction auction;
+    private SniperSnapshot snapshot;
     private boolean isWinning = false;
-    private String itemId;
 
     public AuctionSniper(Auction auction, SniperListener sniperListener, String itemId) {
         this.auction =auction;
         this.sniperListener = sniperListener;
-        this.itemId = itemId;
+        this.snapshot = SniperSnapshot.joining(itemId);
     }
 
     @Override
@@ -25,11 +25,12 @@ public class AuctionSniper implements AuctionEventListener {
     public void currentPrice(int price, int increment, PriceSource priceSource) {
         isWinning = priceSource == PriceSource.FromSniper;
         if (isWinning) {
-            sniperListener.sniperWinning();
+            snapshot = snapshot.winning(price);
         } else {
             final int bid = price + increment;
             auction.bid(bid);
-            sniperListener.sniperStateChanged(new SniperSnapshot(itemId, price, bid, SniperState.BIDDING));
+            snapshot = snapshot.bidding(price, bid);
         }
+        sniperListener.sniperStateChanged(snapshot);
     }
 }
