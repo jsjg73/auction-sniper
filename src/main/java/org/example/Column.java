dd20241550fa3c9e1248a484becda6cdd5f36985
jsjg73 <kjs930703@gmail.com>
2025-03-09 15:51:59 +0900

    경매 행 값 추출 로직을 SnipersTableModel 에서 Column으로 이동.

--- src/main/java/org/example/Column.java
+++ src/main/java/org/example/Column.java
@@ -1,10 +1,34 @@
 package org.example;
 
+import auctionsniper.SniperSnapshot;
+
 public enum Column {
-    ITEM_IDENTIFIER,
-    LAST_PRICE,
-    LAST_BID,
-    SNIPER_STATE;
+    ITEM_IDENTIFIER {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.itemId;
+        }
+    },
+    LAST_PRICE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastPrice;
+        }
+    },
+    LAST_BID {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.lastBid;
+        }
+    },
+    SNIPER_STATE {
+        @Override
+        public Object valueIn(SniperSnapshot snapshot) {
+            return snapshot.state;
+        }
+    };
+
+    abstract public Object valueIn(SniperSnapshot snapShot);
 
     public static Column at(int offset) {
         return values()[offset];
