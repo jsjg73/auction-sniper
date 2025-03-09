962277b955bc2ed9bfee410ec9b629c34c3898c8
jsjg73 <kjs930703@gmail.com>
2025-03-09 15:56:27 +0900

    Column 단위 테스트

---
+++ src/test/java/org/example/ColumnTest.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import auctionsniper.SniperSnapshot;
+import auctionsniper.SniperState;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColumnTest {
+
+    @Test
+    void retrievesValuesFromASniperSnapshot() {
+        SniperSnapshot snapshot = new SniperSnapshot("item-id", 123, 0, SniperState.JOINING);
+
+        assertEquals("item-id", Column.ITEM_IDENTIFIER.valueIn(snapshot));
+        assertEquals(123, Column.LAST_PRICE.valueIn(snapshot));
+        assertEquals(0, Column.LAST_BID.valueIn(snapshot));
+        assertEquals(SniperState.JOINING, Column.SNIPER_STATE.valueIn(snapshot));
+
+    }
+
+}
