ad89b60fa68438050c2010b74060fd379ee0afa0
jsjg73 <kjs930703@gmail.com>
2025-03-17 22:31:00 +0900

    매매 지시 지정 가격 - Item 클래스 도입

---
+++ src/main/java/org/example/Item.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class Item {
+    public final String identifier;
+    public final int stopPrice;
+
+    public Item(String id, int stopPrice) {
+        this.identifier = id;
+        this.stopPrice = stopPrice;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+}
